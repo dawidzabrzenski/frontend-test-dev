970f8f51a70fc7a95fcb50a2d8a2d1151967664c
Zabrzeński Dawid <dawid_zabrzenski@upl.local>
2024-07-10 20:16:52 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+      <img src="html5.svg" alt="HTML5 Logo" className="header__img" />
+      <h2 className="header__text">
+        Zadanie <strong>rekrutacyjne</strong>
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,13 @@
+// import "../styles/components/mainsection.scss";
+
+function MainSection() {
+  return (
+    <section className="section">
+      <div className="s1">1</div>
+      <div className="s2">2</div>
+      <div className="s3">3</div>
+    </section>
+  );
+}
+
+export default MainSection;
